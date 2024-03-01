a6b584b000fbd1ab9ed55ac68a4ff3ddee6c55b3
Ivan <ivan.emidio@live.com.pt>
2024-03-01 00:34:42 +0000

    Booleanos, divisao, shift e xor

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -14,6 +14,21 @@ namespace SmoothOperator
             Console.WriteLine($"Incremento: {++x}");
 
 
+            /// Exercicio 4
+            
+            Console.WriteLine($"Divisao por 2: {x/2}");
+            Console.WriteLine($"Shift Left por 3: {x<<3}");
+            Console.WriteLine($"Xor com 5: {x^5}");
+
+            if (x < 10)
+            {
+            Console.WriteLine("False");
+            }
+            else 
+            {
+            Console.Write("True");
+            }
+
 
         }
     }
