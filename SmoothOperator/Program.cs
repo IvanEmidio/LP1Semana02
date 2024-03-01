ff152cd6131eeb4ee4abfc243edef5b0a6e85f2e
Ivan <ivan.emidio@live.com.pt>
2024-03-01 00:23:46 +0000

    Decremento e Incremento

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Insere numero inteiro nao negativo: ");
+            byte x = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Decremento: {--x} ");
+            Console.WriteLine($"Incremento: {++x}");
+
+
+
+        }
+    }
+}
