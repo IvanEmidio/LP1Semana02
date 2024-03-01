35a92a4d99fd841b615be6867e889736a15bc2e7
Ivan <ivan.emidio@live.com.pt>
2024-03-01 01:02:57 +0000

    Min Valores

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -33,7 +33,33 @@ namespace ValoresEspeciais
             Console.WriteLine($"double: {db}");
             Console.WriteLine($"decimal: {dec}");
 
+            sbyte sbt1 = sbyte.MinValue;
+            short sh1 = short.MinValue;
+            int inte1 = int.MinValue;
+            long lg1 = long.MinValue;
+            byte bt1 = byte.MinValue;
+            ushort ush1 = ushort.MinValue;
+            uint uinte1 = uint.MinValue;
+            ulong ulg1 = ulong.MinValue;
+            float fl1 = float.MinValue;
+            double db1 = double.MinValue;
+            decimal dec1 = decimal.MinValue;
+
+            Console.WriteLine("MinValues: ");
+            Console.WriteLine($"sbyte: {sbt1} ");
+            Console.WriteLine($"short : {sh1} ");
+            Console.WriteLine($"int: {inte1}");
+            Console.WriteLine($"long: {lg1}");
+            Console.WriteLine($"byte: {bt1}");
+            Console.WriteLine($"ushort: {ush1}");
+            Console.WriteLine($"uint: {uinte1}");
+            Console.WriteLine($"ulong: {ulg1}");
+            Console.WriteLine($"float: {fl1}");
+            Console.WriteLine($"double: {db1}");
+            Console.WriteLine($"decimal: {dec1}");
             
+
+
         }
     }
 }
