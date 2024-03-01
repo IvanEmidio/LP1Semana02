7d544635b5ff50ea5e8dbe7a1c1602c4312593aa
Ivan <ivan.emidio@live.com.pt>
2024-03-01 00:38:02 +0000

    Altura e Raio

---
+++ Cilindro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Altura do cilindro: ");
+            float a = float.Parse(Console.ReadLine());
+
+            Console.Write("Raio do cilindro: ");
+            float r = float.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
