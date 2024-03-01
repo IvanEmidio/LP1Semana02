b790c9153ce20b96c64fc268abb496cc0da179f2
Ivan <ivan.emidio@live.com.pt>
2024-03-01 00:40:59 +0000

    Calculos do Cilindro

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -12,6 +12,11 @@ namespace Cilindro
             Console.Write("Raio do cilindro: ");
             float r = float.Parse(Console.ReadLine());
 
+            ///Volume do Cilindro
+            Console.WriteLine($"Volume do cilindro: {MathF.PI * MathF.Pow(r, 2) * a}");
+
+            /// Superficie do Cilindro
+            Console.WriteLine($"A supreficie do cilindro: {2 * MathF.PI * r * (r + a)}");
             
         }
     }
