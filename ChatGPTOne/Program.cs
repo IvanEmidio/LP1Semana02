39af4ee1997f36fbec7bff60b9520902b41ba439
Ivan <ivan.emidio@live.com.pt>
2024-03-01 01:54:19 +0000

    Perguntas ao ChatGPT

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             do
+            {
+                Console.Write("What's in your mind? ");
+                chat = Console.ReadLine();
+                
+                if (chat == "Are you an AI?")
+                {
+                    res = "They say i am";
+                }
+                else if (chat == "What are you doing?")
+                {
+                    res = "Right now?....answering you....";
+                }
+                else if (chat == "How old are you?")
+                {
+                    res = "i prefer not to answer that";
+                }
+                else if (chat == "can you tell how many fingers i have up right now?")
+                {
+                    res = "no.";
+                }
+                else if (chat == "EXIT")
+                {
+                    break;
+                }
+                else
+                {
+                    res = "Sorry i couldn't understand...";
+                }
+            
+                Console.WriteLine(res);
+
+            }   while (true);
+        }
+    }
+}
