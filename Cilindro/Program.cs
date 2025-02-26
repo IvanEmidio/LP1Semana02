29d7a80fdd3c328f39610cf3fd5ef2ada8775886
IvanEmidio <ivan.emidio@live.com.pt>
2025-02-26 15:16:55 +0000

    Added volume

---
+++ Cilindro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            Console.WriteLine("Altura de um cilindro: ");
+            string alt = Console.ReadLine();
+            double a = double.Parse(alt);
+            Console.WriteLine("Raio de um cilindro: ");
+            string raio = Console.ReadLine();
+            double b = double.Parse(raio);
+            Console.WriteLine("O volume do cilindro é " + pi*(Math.Pow(b , 2)*(a)));
+        }
+    }
+}
